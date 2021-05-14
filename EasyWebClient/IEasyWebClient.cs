35ed9fe49ce2cc9d56fd8eab08da28c958212420
Emmanuel Zamorano <final20@gmail.com>
2021-05-14 11:25:47 -0500

    InitialCommit

---
+++ EasyWebClient/IEasyWebClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyWebClient
+{
+    public interface IEasyWebClient
+    {
+        Uri EndPoint { get; set; }
+        string Get();
+        string Get(params KeyValuePair<string, string>[] queries);
+        string Post(byte[] content);
+        string Post(byte[] content, params KeyValuePair<string, string>[] queries);
+        string Put(byte[] content);
+        string Put(byte[] content, params KeyValuePair<string, string>[] queries);
+        string Delete(params KeyValuePair<string, string>[] queries);
+        string Delete();
+        void AddHeader(string name, string value);
+
+    }
+}
